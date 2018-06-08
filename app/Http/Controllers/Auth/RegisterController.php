9efe7d6d9f8557f009ee5c6529190dcd0aed0732
Xian Qiu <qiuxan@gmail.com>
2018-06-08 12:35:39 +0000

    move user to folder

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -11,7 +11,7 @@ class RegisterController extends Controller
 {
     /*
     |--------------------------------------------------------------------------
-    | Register Controller
+    | Register Controller testing testing
     |--------------------------------------------------------------------------
     |
     | This controller handles the registration of new users as well as their
