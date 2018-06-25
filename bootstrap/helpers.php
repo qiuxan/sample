25c1bea22f30fced2df050edf0d489b635462410
Xian Qiu <qiuxan@gmail.com>
2018-06-25 00:14:04 +0000

    update db config

---
+++ bootstrap/helpers.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xianqiu
+ * Date: 25/6/18
+ * Time: 9:11 AM
+ */
+
+
+function get_db_config()
+{
+    if (getenv('IS_IN_HEROKU')) {
+        $url = parse_url(getenv("DATABASE_URL"));
+
+        return $db_config = [
+            'connection' => 'pgsql',
+            'host' => $url["host"],
+            'database'  => substr($url["path"], 1),
+            'username'  => $url["user"],
+            'password'  => $url["pass"],
+        ];
+    } else {
+        return $db_config = [
+            'connection' => env('DB_CONNECTION', 'mysql'),
+            'host' => env('DB_HOST', 'localhost'),
+            'database'  => env('DB_DATABASE', 'forge'),
+            'username'  => env('DB_USERNAME', 'forge'),
+            'password'  => env('DB_PASSWORD', ''),
+        ];
+    }
+}
